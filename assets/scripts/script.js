be4a03ab2c9b5fecf8c012fcb49646328dce2bd4
RaidyMR <raidymustafid39@gmail.com>
2022-11-28 20:26:27 +0700

    update mobile responsive

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,6 @@
 // DARK MODE
 const header = document.getElementsByTagName('header');
 const footer = document.getElementsByTagName('footer');
-const contact = document.getElementsByClassName('contact');
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
@@ -10,27 +9,27 @@ if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
     
     if (currentTheme === 'dark') {
-        document.body.classList.add("dark-mode");
-        header[0].classList.add('dark-header');
-        header[1].classList.add('dark-header');
-        footer[0].classList.add('dark-footer');
+        document.body.classList.add("dark-main");
+        header[0].classList.add('dark-secondary');
+        header[1].classList.add('dark-secondary');
+        footer[0].classList.add('dark-secondary');
         toggleSwitch.checked = true;
     }
 }
 
 toggleSwitch.addEventListener('change', (e) => {
     if (e.target.checked) {
-        document.body.classList.add("dark-mode");
-        header[0].classList.add('dark-header');
-        header[1].classList.add('dark-header');
-        footer[0].classList.add('dark-footer');
+        document.body.classList.add("dark-main");
+        header[0].classList.add('dark-secondary');
+        header[1].classList.add('dark-secondary');
+        footer[0].classList.add('dark-secondary');
         localStorage.setItem('theme', 'dark');
     }
     else {        
-        document.body.classList.remove("dark-mode");
-        header[0].classList.remove('dark-header');
-        header[1].classList.remove('dark-header');
-        footer[0].classList.remove('dark-footer');
+        document.body.classList.remove("dark-main");
+        header[0].classList.remove('dark-secondary');
+        header[1].classList.remove('dark-secondary');
+        footer[0].classList.remove('dark-secondary');
         localStorage.setItem('theme', 'light');
     }    
 
@@ -78,3 +77,19 @@ const createQuote = (quote) => {
 }
 
 pickQuote();
+
+// CAROUSEL
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
